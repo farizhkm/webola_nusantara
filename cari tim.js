1103964025b510070cdebe413de2c0c7ef48bc1a
meirel chrisna laturake <imanuellaturake@gmail.com>
2024-07-22 18:13:06 +0700

    tambahan

--- cari tim.js
+++ cari tim.js
@@ -1,16 +1,16 @@
 function searchPlayer() {
-    // Get the input value and convert it to lowercase
+    
     var input = document.getElementById('searchInput').value.toLowerCase();
     
-    // Get all team cards
+    
     var cards = document.querySelectorAll('.tim-card');
     
-    // Loop through all cards
+    
     cards.forEach(function(card) {
-        // Get the name of the team from the card's id or data-name attribute
+        
         var teamName = card.querySelector('h2').textContent.toLowerCase();
         
-        // If the input value is found in the team name, show the card, otherwise hide it
+        
         if (teamName.includes(input)) {
             card.style.display = '';
         } else {
